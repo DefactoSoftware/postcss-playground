75797cf73b6c7b84b94fde136e5cc7c50dc82720
Matthijs Kuiper <info@matthijskuiper.nl>
2022-03-16 15:12:43 +0100

    Testing

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,17 @@ module.exports = {
   plugins: {
     "postcss-css-variables": {
       variables: variables,
-      preserveInjectedVariables: false,
+      // preserveInjectedVariables: false,
       preserve: declaration => {
         // console.log(declaration);
 
-        // return declaration.value === "var(--color-primary)";
         return declaration.value.includes("var(--color-primary)");
 
         // const { prop, value } = declaration;
-        // return prop === "--color-primary" || value === "var(--color-primary)";
+        // // return value === "var(--color-primary)" || prop === "--color-primary";
+        // return (
+        //   value.includes("var(--color-primary)") || prop === "--color-primary"
+        // );
       }
     },
     "postcss-color-function": {}
