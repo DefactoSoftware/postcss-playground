4d4a7780de349af463249ad448dfedb1c223aad1
Matthijs Kuiper <info@matthijskuiper.nl>
2022-03-16 15:43:22 +0100

    HSLA implementation

--- postcss.config.js
+++ postcss.config.js
@@ -2,19 +2,20 @@ const variables = require("./shared-variables");
 
 module.exports = {
   plugins: {
+    "postcss-discard-comments": {
+      removeAll: true
+    },
     "postcss-css-variables": {
       variables: variables,
-      // preserveInjectedVariables: false,
       preserve: declaration => {
         // console.log(declaration);
 
-        return declaration.value.includes("var(--color-primary)");
-
-        // const { prop, value } = declaration;
-        // // return value === "var(--color-primary)" || prop === "--color-primary";
-        // return (
-        //   value.includes("var(--color-primary)") || prop === "--color-primary"
-        // );
+        const { prop, value } = declaration;
+        // return value.includes("var(--color-primary");
+        return (
+          value.includes("var(--color-primary") ||
+          prop.startsWith("--color-primary")
+        );
       }
     },
     "postcss-color-function": {}
--- shared-variables.js
+++ shared-variables.js
@@ -1,4 +1,10 @@
 module.exports = {
-  "--color-primary": "#ff0000",
-  "--color-text": "green"
+  "--color-text": "green",
+  // "--color-primary": "#ff0000",
+
+  "--color-primary": "hsla(0, 100%, 50%, 100%)",
+  "--color-primary-hsl": "0, 100%, 50%",
+  "--color-primary-h": "0",
+  "--color-primary-s": "100%",
+  "--color-primary-l": "50%"
 };
