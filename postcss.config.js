45b7eb395178cc8ff41e714932cf7f2efc814e1d
Matthijs Kuiper <info@matthijskuiper.nl>
2022-03-16 13:04:11 +0100

    Working preserve where the fallback is also saved as css variable

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,13 @@ module.exports = {
   plugins: {
     "postcss-css-variables": {
       variables: variables,
+      // preserveInjectedVariables: false,
       preserve: declaration => {
         // console.log(declaration);
-        return declaration.value === "var(--color-primary)";
+        // return declaration.value === "var(--color-primary)";
+
+        const { prop, value } = declaration;
+        return prop === "--color-primary" || value === "var(--color-primary)";
       }
     },
     "postcss-color-function": {}
